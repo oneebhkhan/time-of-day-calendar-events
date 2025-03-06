f5907be41b5fba971ddbd91323312f0d87ecea35
Oneeb <64619727+oneebhkhan@users.noreply.github.com>
2025-03-07 01:29:54 +0300

    feat: ramzan calendar events

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # time-of-day-calendar-events
+
+Script that generates calendar events according to time of day. Some applicable use cases are creating calendar events for:
+- Sunrise / Sunset
+- Suhoor and Iftar times, 
+- Prayer times
+
+## How to use
+Clone the repository locally and run `python <script_name>.py ––days 30 ––event_type sun --duration 5m --location country-code/city-code`  
+
+
+### Todo:
+Use fire library
---
+++ main.py
@@ -0,0 +1,76 @@
+
+import fire
+import pytz
+import requests
+from ics import Calendar, Event
+from datetime import timedelta, datetime
+
+
+def get_lat_lon(location):
+    request = requests.get(f"https://geocode.xyz/{location}?json=1")
+
+    if request.status_code == 200:
+        data = request.json()
+        lat = data["latt"]
+        lon = data["longt"]
+
+        return lat, lon
+    else:
+        raise Exception("Failed to get lat/lon")
+
+
+def get_prayer_times(date, lat, lon, method):
+    request = requests.get(f"http://api.aladhan.com/v1/timings/{date}?latitude={lat}&longitude={lon}")
+
+    if request.status_code == 200:
+        data = request.json()
+        time_zone = pytz.timezone(data["data"]["meta"]["timezone"])
+        sehri_timing = data["data"]["timings"]["Fajr"]
+        iftar_timing = data["data"]["timings"]["Maghrib"]
+
+        return sehri_timing, iftar_timing, time_zone
+    else:
+        raise Exception("Failed to get prayer times")
+
+
+# create a calendar event
+def create_event(start_time, duration, name, location):
+    event = Event()
+    event.name = name
+    event.begin = start_time
+    event.duration = duration
+    event.location = location
+
+    return event
+
+
+# save events to calendar
+def create_calendar(start_date, num_events, duration, location, fiqah):
+
+    lat, lon = get_lat_lon(location)
+
+    events = []
+
+    for i in range(num_events):
+        date = (datetime.strptime(start_date, "%d-%m-%Y") + (i * timedelta(days=1))).strftime("%d-%m-%Y")
+        suhoor_time, iftar_time, time_zone = get_prayer_times(date, lat, lon, fiqah)
+
+        converted_date = datetime.strptime(date, "%d-%m-%Y").strftime("%Y-%m-%d")
+        suhoor_time_dt = time_zone.localize(datetime.strptime(f"{converted_date} {suhoor_time}", "%Y-%m-%d %H:%M"))
+        iftar_time_dt = time_zone.localize(datetime.strptime(f"{converted_date} {iftar_time}", "%Y-%m-%d %H:%M"))
+
+        suhoor_event = create_event(suhoor_time_dt, timedelta(minutes=duration), f"Suhoor@{location}  ", location)
+        iftar_event = create_event(iftar_time_dt, timedelta(minutes=duration), f"Iftar@{location}", location)
+
+        events.append(suhoor_event)
+        events.append(iftar_event)
+
+    cal = Calendar()
+    cal.events = events
+
+    # save the calendar to a file
+    with open(f"Ramzan@{location}-{fiqah}.ics", "w") as f:
+        f.writelines(cal)
+
+
+fire.Fire(create_calendar)
